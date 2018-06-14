68a7e06f396a7883195645e8ef65aa3884e7a781
Percifull <noahpercifull@yahoo.de>
2018-06-14 16:37:33 +0200

    Changelog: ControllerImpl: Fixed Pagination, henceforth only loading messages for requested page,  instead of loading all of them and then filtering wanted messages. MessageRepository: Implementing functions for changes in ControllerImpl as described above. messages.html: no difference, except popup example in a comment own_messages: Fixed bug: page-navigation redirected to messages.html

--- spring-redis-demo/src/main/java/de/hska/lkit/demo/redis/repo/impl/MessageRepositoryImpl.java
+++ spring-redis-demo/src/main/java/de/hska/lkit/demo/redis/repo/impl/MessageRepositoryImpl.java
@@ -1,17 +1,15 @@
 package de.hska.lkit.demo.redis.repo.impl;
 
-import de.hska.lkit.demo.redis.model.Impl.Message;
+import de.hska.lkit.demo.redis.model.Message;
 import de.hska.lkit.demo.redis.model.SimpleSecurity;
 
-import de.hska.lkit.demo.redis.model.Impl.User;
+import de.hska.lkit.demo.redis.model.User;
 import de.hska.lkit.demo.redis.repo.MessageRepository;
 
-import de.hska.lkit.demo.redis.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.RedisServerCommands;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.support.atomic.RedisAtomicLong;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
@@ -39,9 +37,7 @@ public class MessageRepositoryImpl implements MessageRepository {
 
 	private static final String KEY_FOLLOWING_USER = "m:following:";
 
-    private final UserRepository userRepository;
-
-    /**
+	/**
 	 * to generate unique ids for message
 	 */
 	private RedisAtomicLong m_id;
@@ -56,9 +52,7 @@ public class MessageRepositoryImpl implements MessageRepository {
 	 */
 	private RedisTemplate<String, Object> redisTemplate;
 
-    private final SimpMessagingTemplate messagingTemplate;
-
-    /**
+	/**
 	 * for simple value Operations
 	 */
 
@@ -92,13 +86,11 @@ public class MessageRepositoryImpl implements MessageRepository {
 
 
 	@Autowired
-	public MessageRepositoryImpl(UserRepository userRepository, RedisTemplate<String, Object> redisTemplate, StringRedisTemplate stringRedisTemplate, SimpMessagingTemplate messagingTemplate) {
-		this.userRepository = userRepository;
+	public MessageRepositoryImpl(RedisTemplate<String, Object> redisTemplate, StringRedisTemplate stringRedisTemplate) {
 		this.redisTemplate = redisTemplate;
 		this.stringRedisTemplate = stringRedisTemplate;
 		this.m_id = new RedisAtomicLong("m_id", stringRedisTemplate.getConnectionFactory());
-        this.messagingTemplate = messagingTemplate;
-    }
+	}
 	
 	@PostConstruct
 	private void init() {
@@ -124,29 +116,7 @@ public class MessageRepositoryImpl implements MessageRepository {
 	}
 
 	@Override
-    public void post(String m_key) {
-        Message message = rt_hashOps.get(KEY_LIST_MESSAGE_GLOBAL, m_key);
-        String autorID = userRepository.getIdByName(message.getAutor());
-        User autor = userRepository.getUserById(autorID);
-        Map<String, User> followers = userRepository.getFollowers(autorID);
-
-        if (autor.isOnline()) {
-            messagingTemplate.convertAndSend("/newMessages/" + autor.getUsername(), message); //websocket
-			System.out.println("MessageRepositoryImpl:post(m_key) : message sent via stomp to autor");
-        }
-
-        for (User follower : followers.values()) {
-            if (follower.isOnline()) {
-                messagingTemplate.convertAndSend("/newMessages/" + follower.getUsername(), message); //websocket
-                System.out.println("MessageRepositoryImpl:post(m_key) : message sent via stomp to follower: " + follower.getUsername() + " who is online.");
-            } else {
-                System.out.println("MessageRepositoryImpl:post(m_key) : message not sent to follower: " + follower.getUsername() + " because he is not online.");
-            }
-        }
-    }
-
-	@Override
-	public Message postMessage(String text) {
+	public void postMessage(String text, Map<String, User> followers) {
 
 		String m_id = String.valueOf(this.m_id.incrementAndGet());
         String u_id = SimpleSecurity.getUid();
@@ -168,9 +138,9 @@ public class MessageRepositoryImpl implements MessageRepository {
 		//global timeline message ID's (all)
 		srt_listOps.leftPush(KEY_LIST_MESSAGE_GLOBAL, key);
         //own timeline messages (own + following)
-        srt_listOps.leftPush(KEY_LIST_MESSAGE_USER + u_id, key);   //TWICE?
+        srt_listOps.leftPush(KEY_LIST_MESSAGE_USER + u_id, key);
         //only own message ID's
-		//srt_listOps.leftPush(KEY_LIST_MESSAGE_USER + u_id, key);   //^ ???
+		srt_listOps.leftPush(KEY_LIST_MESSAGE_USER + u_id, key);
 
 		//global timeline messages (all)
 		rt_hashOps.put(KEY_LIST_MESSAGE_GLOBAL, key, message);
@@ -186,12 +156,9 @@ public class MessageRepositoryImpl implements MessageRepository {
         }*/
         //add message to all follower lists
         //here is the Redis.publish to use for signaling incoming message in followers session
-        Map<String, User> followers = userRepository.getFollowers(u_id);
         for (User follower : followers.values()) {
             srt_listOps.leftPush(KEY_LIST_MESSAGE_USER + getIDByKey(follower.getId()), key);
-		}
-        redisTemplate.convertAndSend("tweetReceived", key);
-        return message;
+        }
     }
 
     public String getIDByKey(String key) {
@@ -235,7 +202,7 @@ public class MessageRepositoryImpl implements MessageRepository {
     }
 
     @Override
-    public List<Message> getMessagesInRange(String userID, int start, int end, MessageRepository messageRepository) {
+    public List<Message> getMessagesInRange(String userID,int start, int end, MessageRepository messageRepository) {
         List<String> ids = getMessageIDsInRange(userID, start, end);
         List<Message> messages = new ArrayList<>();
         for (String id : ids) {
